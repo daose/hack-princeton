6df6ebc2f7c52492f7b198f3e1957ab011568804
daose <bychen@uwaterloo.ca>
2016-11-12 07:32:55 -0500

    logging

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.get('/', function (req, res) {
 })
 
 app.get('/webhook/', function (req, res) {
+    console.log("request: ", req);
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
