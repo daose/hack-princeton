93ddce45075eff7ed1886b62e2533bfff2a0797a
daose <bychen@uwaterloo.ca>
2016-11-12 10:23:41 -0500

    firebase test

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 'use strict'
 
+const admin = require('firebase-admin')
+const serviceAccount = require('hack-princeton-firebase-adminsdk-x9bdc-d39b75de21.json')
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 const users = ['1077146965714361', '1253903971335322']
 const token = 'EAAXQIOPTDSsBAExBqmK0OpIC8ARLpVRZBeuM3FbYjeEN7rYJCO1rs9FLZBbjbncAZCEVfunLhH5ABOwYJqnOb5E2vVKTuihuN7ZBk0uAhZBiPlJ2tHZBIrwlhvJyh01zhO0Le1O9rZAhy2ZAhZBcLZCXxjX5caXXVTMVekMeJm2lcGbQZDZD'
+
+//Firebase Init
+admin.initializeApp({
+     credential: admin.credential.cert(serviceAccount),
+     databaseURL: "https://hack-princeton.firebaseio.com"
+});
+
+var db = admin.database();
+var dbRef = db.ref("bot");
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -22,11 +33,12 @@ app.get('/', function (req, res) {
 
 app.post('/webhook/', function (req, res) {
     let events = req.body.entry[0].messaging
+    console.log(events);
     for(let i = 0; i < events.length; i++){
         let event = req.body.entry[0].messaging[i];
         let sender = event.sender.id;
         if(event.postback){
-            handlePostback(sender);
+            handlePostback(sender, event.postback);
         } else {
             if(sender === users[0]){
                 console.log("temp master sender detected");
@@ -37,8 +49,9 @@ app.post('/webhook/', function (req, res) {
     res.sendStatus(200)
 });
 
-function handlePostback(senderId){
-    console.log("postback sender id: ", senderId);
+function handlePostback(sender, postback){
+    var userRef = dbRef.child(sender);
+    userRef.set(postback);
 }
 
 function broadcastMessage(sender) {
