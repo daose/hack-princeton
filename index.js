a75b203be97e5f091429259eff098c716789a855
daose <bychen@uwaterloo.ca>
2016-11-12 09:49:41 -0500

    forgot to send request

--- index.js
+++ index.js
@@ -76,7 +76,19 @@ function sendPromptMessage(senderId, messageText) {
                 }
             }
         }
-    }
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: token },
+        method: 'POST',
+        json: messageData
+    }, function(error, response, body) {
+        if(error){
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
 }
  
 function sendTextMessage(recipientId, messageText) {
