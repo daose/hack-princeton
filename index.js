a54cc4d704d46e7511070285ac10c130012e5c4b
daose <bychen@uwaterloo.ca>
2016-11-12 15:27:03 -0500

    parse float

--- index.js
+++ index.js
@@ -95,16 +95,21 @@ function broadcastMessage(sender, imagePayload) {
 }
 
 function ocrOnResponse(body) {
+    var totalAmount;
     for(var i in body) {
         if(typeof body[i] === 'object'){
             ocrOnResponse(body[i]);
         } else {
             var value = body[i].toString();
             if(value.match(rePattern)){
-                console.log("leaf: " + body[i]);
+                var amount = parseFloat(body[i]);
+                if(amount > totalAmount){
+                    totalAmount = amount;
+                }
             }
         }
     }
+    console.log(totalAmount);
 }
 
 function sendPromptMessage(senderId, messageText) {
